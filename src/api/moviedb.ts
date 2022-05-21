bcac15f66d1dbd66ec60fc4a3ff3d5a5bb3dbf6a
malith <malithpatabandige@gmail.com>
2022-05-22 01:37:57 +1200

    created spacers to fix alignment issues

--- src/api/moviedb.ts
+++ src/api/moviedb.ts
@@ -25,57 +25,106 @@ export type SearchResultsItem = {
 };
 
 export interface GetMovieResponse {
-    adult: boolean;
-    backdrop_path: string;
-    belongs_to_collection: BelongsToCollectionType;
-    budget: number;
-    genres?: (GenresEntity)[] | null;
-    homepage: string;
-    id: number;
-    imdb_id: string;
-    original_language: string;
-    original_title: string;
-    overview: string;
-    popularity: number;
-    poster_path: string;
-    production_companies?: (ProductionCompaniesEntity)[] | null;
-    production_countries?: (ProductionCountriesEntity)[] | null;
-    release_date: string;
-    revenue: number;
-    runtime: number;
-    spoken_languages?: (SpokenLanguagesEntity)[] | null;
-    status: string;
-    tagline: string;
-    title: string;
-    video: boolean;
-    vote_average: number;
-    vote_count: number;
-  }
-  export interface BelongsToCollectionType {
-    id: number;
-    name: string;
-    poster_path: string;
-    backdrop_path: string;
-  }
-  export interface GenresEntity {
-    id: number;
-    name: string;
-  }
-  export interface ProductionCompaniesEntity {
-    id: number;
-    logo_path: string;
-    name: string;
-    origin_country: string;
-  }
-  export interface ProductionCountriesEntity {
-    iso_3166_1: string;
-    name: string;
-  }
-  export interface SpokenLanguagesEntity {
-    english_name: string;
-    iso_639_1: string;
-    name: string;
-  }
+  adult: boolean;
+  backdrop_path: string;
+  belongs_to_collection: BelongsToCollectionType;
+  budget: number;
+  genres?: (GenresEntity)[] | null;
+  homepage: string;
+  id: number;
+  imdb_id: string;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  production_companies?: (ProductionCompaniesEntity)[] | null;
+  production_countries?: (ProductionCountriesEntity)[] | null;
+  release_date: string;
+  revenue: number;
+  runtime: number;
+  spoken_languages?: (SpokenLanguagesEntity)[] | null;
+  status: string;
+  tagline: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+export interface BelongsToCollectionType {
+  id: number;
+  name: string;
+  poster_path: string;
+  backdrop_path: string;
+}
+export interface GenresEntity {
+  id: number;
+  name: string;
+}
+export interface ProductionCompaniesEntity {
+  id: number;
+  logo_path: string;
+  name: string;
+  origin_country: string;
+}
+export interface ProductionCountriesEntity {
+  iso_3166_1: string;
+  name: string;
+}
+export interface SpokenLanguagesEntity {
+  english_name: string;
+  iso_639_1: string;
+  name: string;
+}
+
+export interface ConfigurationResponse {
+  images: Images;
+  change_keys?: (string)[] | null;
+}
+export interface Images {
+  base_url: string;
+  secure_base_url: string;
+  backdrop_sizes?: (string)[] | null;
+  logo_sizes?: (string)[] | null;
+  poster_sizes?: (string)[] | null;
+  profile_sizes?: (string)[] | null;
+  still_sizes?: (string)[] | null;
+}
+
+export interface CreditsResponse {
+  id: number;
+  cast?: (CastEntity)[] | null;
+  crew?: (CrewEntity)[] | null;
+}
+export interface CastEntity {
+  adult: boolean;
+  gender: number;
+  id: number;
+  known_for_department: string;
+  name: string;
+  original_name: string;
+  popularity: number;
+  profile_path?: string | null;
+  cast_id: number;
+  character: string;
+  credit_id: string;
+  order: number;
+}
+export interface CrewEntity {
+  adult: boolean;
+  gender: number;
+  id: number;
+  known_for_department: string;
+  name: string;
+  original_name: string;
+  popularity: number;
+  profile_path?: string | null;
+  credit_id: string;
+  department: string;
+  job: string;
+}
+  
+  
   
 
 export const searchMovie = async (query: string): Promise<SearchResultsItem[] | null | undefined> => {
@@ -84,7 +133,7 @@ export const searchMovie = async (query: string): Promise<SearchResultsItem[] | 
     if (response.ok) {
         const searchResults = results;
         console.log(searchResults);
-        return searchResults?.slice(0, 4);
+        return searchResults?.slice(0, 5);
     }
     return null;
 };
@@ -97,4 +146,23 @@ export const getMovieDetail = async (movie_id: string): Promise<GetMovieResponse
         return movieResponse;
     }
     return null;
+};
+
+export const getConfiguration = async (): Promise<ConfigurationResponse | null> => {
+  const response = await fetch(`https://api.themoviedb.org/3/configuration?api_key=${apiKey}`);
+  const configResponse: ConfigurationResponse = await response.json();
+  if (response.ok) {
+    return configResponse;
+  }
+  return null;
+};
+
+export const getCredits = async(movie_id: string): Promise<CastEntity[] | undefined> => {
+  const response = await fetch(`https://api.themoviedb.org/3/movie/${movie_id}/credits?api_key=${apiKey}`);
+  const creditsResponse: CreditsResponse = await response.json();
+  if (response.ok) {
+    const cast = creditsResponse.cast;
+    return cast?.sort((a,b) => b.popularity - a.popularity).slice(0, 5);
+  }
+  return undefined;
 };
--- src/components/MovieDetails.tsx
+++ src/components/MovieDetails.tsx
@@ -1,9 +1,16 @@
 import React, { useState } from "react";
 import { useParams } from "react-router-dom";
-import { getMovieDetail, GetMovieResponse } from "../api/moviedb";
+import { CastEntity, ConfigurationResponse, CreditsResponse, CrewEntity, getConfiguration, getCredits, getMovieDetail, GetMovieResponse } from "../api/moviedb";
+import '../styles/MovieDetails.css';
+
+const Spacer = () => (
+    <div style={{width: "10%"}}></div>
+);
 
 const MovieDetails = () => {
     const [movieDetails, setMovieDetails] = useState<GetMovieResponse>();
+    const [config, setConfig] = useState<ConfigurationResponse>();
+    const [cast, setCast] = useState<CastEntity[]>();
     let {id} = useParams();
     
     React.useEffect(() => {
@@ -12,14 +19,65 @@ const MovieDetails = () => {
             .then(response => {
                 if (response) {
                     setMovieDetails(response);
-                    console.log(response);
                 }
             });
+
+            getCredits(id)
+            .then(response => {
+                if (response) {
+                    setCast(response);
+                    console.log(response);
+                }
+            })
         }
+        
+        getConfiguration()
+        .then (response => {
+            if (response) {
+                setConfig(response);
+            }
+        })
     }, []);
 
     return (
-        <h1>Movie details for {id}</h1>
+        <div className="detailsBody">
+            <div className="spacer"></div>
+            <div className="mainDiv">
+                <h1>{movieDetails?.title}</h1>
+                <div className="infoContainer">
+                    <img src={`${config?.images.base_url}/w500/${movieDetails?.poster_path}`} alt="" width="300px"/>
+                    <div >
+                        <div className="infoDiv">
+                            <table>
+                                <tr>
+                                    <td>Release Date:</td>
+                                    <td className="tableData">{movieDetails?.release_date}</td>
+                                </tr>
+                                <tr>
+                                    <td>Runtime:</td>
+                                    <td className="tableData">{movieDetails?.runtime}</td>
+                                </tr>
+                                <tr>
+                                    <td>Top Cast:</td>
+                                    <td className="tableData">
+                                            {cast?.map(actor => (
+                                                <div>{actor.name}</div>
+                                            ))}
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                </div>                
+                <div className="overviewContainer">
+                    <div>
+                        {movieDetails?.overview}
+                    </div>
+                </div>
+            </div>
+            <div className="spacer"></div>
+        </div>
+        
     );
 };
 
