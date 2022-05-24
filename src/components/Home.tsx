ad47b60b067a8c1738e6bf685d6f6370995b3298
malith <malithpatabandige@gmail.com>
2022-05-24 23:28:48 +1200

    updated titles

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,7 +3,7 @@ import SearchBox from "./SearchBox";
 const Home = () => {
     return (
         <div className="App">
-        <h2>Little test - landing page</h2>
+        <h2>Awesome Movie Database</h2>
         <SearchBox />
         </div>
     );
